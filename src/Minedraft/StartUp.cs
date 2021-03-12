842590a2ddf160c1fa3b1357b1d04b48a49618d7
KrusteF <79479041+KrusteF@users.noreply.github.com>
2021-03-12 12:33:20 +0200

    Updated StartUp.cs

--- src/Minedraft/StartUp.cs
+++ src/Minedraft/StartUp.cs
@@ -8,20 +8,25 @@
         public static void Main()
         {
             DraftManager manager = new DraftManager();
+
             while (true)
             {
                 string input = Console.ReadLine();
                 string[] str = input.Split(" ");
                 string command = str[0];
+
                 List<string> args = new List<string>(str);
                 args.RemoveAt(0);
                 object[] prms = new[] { args };
+
                 if (args.Count == 0)
                 {
                     prms = new object[0];
                 }
+
                 string output = (string)manager.GetType().GetMethod(command).Invoke(manager, prms);
                 Console.WriteLine(output);
+                
                 if (command.Equals("Shutdown"))
                 {
                     break;
